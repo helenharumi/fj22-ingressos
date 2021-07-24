bb3a8ec9f8c1dee77bc5cf8be398bac6779634f4
Helen Harumi <helen.harumi1@gmail.com>
2021-07-24 16:33:13 -0300

    feature: melhorando usabilidade do carrinho

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
+import br.com.caelum.ingresso.dao.LugarDao;
+
 @Component
 @SessionScope
 public class Carrinho {
-	
+
 	private List<Ingresso> ingressos = new ArrayList<>();
-	
+
 	public void add(Ingresso ingresso) {
 		ingressos.add(ingresso);
 	}
@@ -23,5 +25,9 @@ public class Carrinho {
 	public void setIngressos(List<Ingresso> ingressos) {
 		this.ingressos = ingressos;
 	}
-	
+
+	public boolean isSelecionado(Lugar lugar) {
+		return ingressos.stream().map(Ingresso::getLugar).anyMatch(lugarDoIngresso -> lugarDoIngresso.equals(lugar));
+	}
+
 }
