928c88782ad5e34d73d453dd30405dff2670ea85
Helen Harumi <helen.harumi1@gmail.com>
2021-07-24 14:50:57 -0300

    feature: selecao de lugares, ingressos e tipo de ingressos

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,14 +3,18 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.ManyToOne;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
@@ -29,10 +33,20 @@ public class Sessao {
 	
 	private BigDecimal preco;
 	
+	@OneToMany(mappedBy	= "sessao",	fetch = FetchType.EAGER)
+	private	Set<Ingresso> ingressos	= new HashSet<>();
+	
+	  /**
+     * @deprecated hibernate only
+     */
 	public Sessao() {
 		
 	}
 	
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
@@ -84,4 +98,12 @@ public class Sessao {
 		this.filme = filme;
 	}
 
+	public Set<Ingresso> getIngressos() {
+		return ingressos;
+	}
+
+	public void setIngressos(Set<Ingresso> ingressos) {
+		this.ingressos = ingressos;
+	}
+	
 }
